f0b2479898fd04c645487ff0649f5ff2b58bc679
tomvanmele <tom.v.mele@gmail.com>
2024-09-05 23:37:02 +0200

    scene objects

---
+++ src/compas_rv/scene/__init__.py
@@ -0,0 +1,20 @@
+from compas.plugins import plugin
+from compas.scene.context import register
+
+from compas_rv.datastructures import ForceDiagram
+from compas_rv.datastructures import FormDiagram
+from compas_rv.datastructures import Pattern
+from compas_rv.datastructures import ThrustDiagram
+
+from .forceobject import RhinoForceObject
+from .formobject import RhinoFormObject
+from .patternobject import RhinoPatternObject
+from .thrustobject import RhinoThrustObject
+
+
+@plugin(category="factories", pluggable_name="register_scene_objects", requires=["Rhino"])
+def register_scene_objects_rhino():
+    register(ForceDiagram, RhinoForceObject, context="Rhino")
+    register(FormDiagram, RhinoFormObject, context="Rhino")
+    register(Pattern, RhinoPatternObject, context="Rhino")
+    register(ThrustDiagram, RhinoThrustObject, context="Rhino")
---
+++ src/compas_rv/scene/forceobject.py
@@ -0,0 +1,71 @@
+from compas.colors import Color
+from compas.scene.descriptors.color import ColorAttribute
+from compas_rui.scene import RUIMeshObject
+from compas_rv.datastructures import ForceDiagram
+
+
+class RhinoForceObject(RUIMeshObject):
+    mesh: ForceDiagram
+
+    freecolor = ColorAttribute(default=Color.white())
+    anchorcolor = ColorAttribute(default=Color.red())
+    fixedcolor = ColorAttribute(default=Color.blue())
+
+    def __init__(
+        self,
+        show_anchors=True,
+        show_fixed=True,
+        show_free=False,
+        disjoint=True,
+        **kwargs,
+    ):
+        super().__init__(disjoint=disjoint, **kwargs)
+
+        self.show_anchors = show_anchors
+        self.show_fixed = show_fixed
+        self.show_free = show_free
+
+    @property
+    def settings(self):
+        settings = super().settings
+
+        settings["show_anchors"] = self.show_anchors
+        settings["show_fixed"] = self.show_fixed
+        settings["show_free"] = self.show_free
+
+        return settings
+
+    def draw(self):
+        for vertex in self.mesh.vertices():
+            if self.mesh.vertex_attribute(vertex, "is_anchor"):
+                self.vertexcolor[vertex] = self.anchorcolor
+            elif self.mesh.vertex_attribute(vertex, "is_fixed"):
+                self.vertexcolor[vertex] = self.fixedcolor
+            else:
+                self.vertexcolor[vertex] = self.freecolor
+
+        super().draw()
+        return self.guids
+
+    def draw_vertices(self):
+        vertices = []
+
+        if self.show_free:
+            vertices += list(self.mesh.vertices_where(is_anchor=False, is_fixed=False))
+        if self.show_fixed:
+            vertices += list(self.mesh.vertices_where(is_fixed=True))
+        if self.show_anchors:
+            vertices += list(self.mesh.vertices_where(is_anchor=True))
+
+        if vertices:
+            self.show_vertices = vertices
+
+        for vertex in self.mesh.vertices():
+            if self.mesh.vertex_attribute(vertex, "is_anchor"):
+                self.vertexcolor[vertex] = self.anchorcolor
+            elif self.mesh.vertex_attribute(vertex, "is_fixed"):
+                self.vertexcolor[vertex] = self.fixedcolor
+            else:
+                self.vertexcolor[vertex] = self.freecolor
+
+        return super().draw_vertices()
