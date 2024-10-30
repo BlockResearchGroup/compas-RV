8350e341833371377199f24b2c4ffd8eae583deb
tomvanmele <tom.v.mele@gmail.com>
2024-10-30 18:40:56 +0100

    same setup as form finder

--- src/compas_rv/scene/patternobject.py
+++ src/compas_rv/scene/patternobject.py
@@ -13,53 +13,17 @@ class RhinoPatternObject(RUIMeshObject):
 
     def __init__(
         self,
-        show_supports=True,
-        show_fixed=True,
-        show_free=False,
         disjoint=True,
         **kwargs,
     ):
         super().__init__(disjoint=disjoint, **kwargs)
 
-        self.show_supports = show_supports
-        self.show_fixed = show_fixed
-        self.show_free = show_free
-
     @property
     def settings(self):
         settings = super().settings
-
-        settings["show_supports"] = self.show_supports
-        settings["show_fixed"] = self.show_fixed
-        settings["show_free"] = self.show_free
-
         return settings
 
-    def draw(self):
-        for vertex in self.mesh.vertices():
-            if self.mesh.vertex_attribute(vertex, "is_support"):
-                self.vertexcolor[vertex] = self.anchorcolor
-            elif self.mesh.vertex_attribute(vertex, "is_fixed"):
-                self.vertexcolor[vertex] = self.fixedcolor
-            else:
-                self.vertexcolor[vertex] = self.freecolor
-
-        super().draw()
-        return self.guids
-
     def draw_vertices(self):
-        vertices = []
-
-        if self.show_free:
-            vertices += list(self.mesh.vertices_where(is_support=False, is_fixed=False))
-        if self.show_fixed:
-            vertices += list(self.mesh.vertices_where(is_fixed=True))
-        if self.show_supports:
-            vertices += list(self.mesh.vertices_where(is_support=True))
-
-        if vertices:
-            self.show_vertices = vertices
-
         for vertex in self.mesh.vertices():
             if self.mesh.vertex_attribute(vertex, "is_support"):
                 self.vertexcolor[vertex] = self.anchorcolor
