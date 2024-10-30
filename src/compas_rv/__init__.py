dfb609a6de24498132bd60eb8ce8ae4e85872137
tomvanmele <tom.v.mele@gmail.com>
2024-10-30 18:42:01 +0100

    not needed with new splash

--- src/compas_rv/__init__.py
+++ src/compas_rv/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import os
 
 
@@ -8,14 +6,6 @@ __copyright__ = "ETH Zurich - Block Research Group"
 __license__ = "MIT License"
 __email__ = "tom.v.mele@gmail.com"
 __version__ = "0.1.0"
-
-
-designers = []
-developers = []
-documenters = []
-title = "RhinoVAULT"
-website = "https://blockresearchgroup.gitbook.io/rhinovault"
-description = "Rhino plugin for form finding of compression-only structures using Thrust Network Analysis."
 
 
 HERE = os.path.dirname(__file__)
