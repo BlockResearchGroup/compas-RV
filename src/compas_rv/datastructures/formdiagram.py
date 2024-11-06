e354ba4b156dbcc19ae5e4457c346cfccbb3c072
tomvanmele <tom.v.mele@gmail.com>
2024-11-07 00:17:49 +0100

    true boundaries

--- src/compas_rv/datastructures/formdiagram.py
+++ src/compas_rv/datastructures/formdiagram.py
@@ -28,3 +28,14 @@ class FormDiagram(Diagram, FormDiagram):
         form: FormDiagram = pattern.copy(cls=cls)
         form.update_boundaries()
         return form
+
+    # because it might clash with the parent function
+    def edges_on_boundaries(self):
+        boundaries = []
+        for face in self.faces_where(_is_loaded=False):
+            boundary = []
+            for edge in self.face_halfedges(face):
+                if self.edge_attribute(edge, name="_is_edge"):
+                    boundary.append(edge)
+            boundaries.append(boundary)
+        return boundaries
