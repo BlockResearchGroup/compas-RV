ba0c8d30a00c12bff28510b8ecdb2a5fdbf0e0ef
tomvanmele <tom.v.mele@gmail.com>
2024-11-07 00:19:24 +0100

    thrust

--- plugin/RV_thrust_modify.py
+++ plugin/RV_thrust_modify.py
@@ -2,18 +2,23 @@
 # venv: rhinovault
 # r: compas>=2.5, compas_rui>=0.3.1, compas_session>=0.4.1, compas_tna>=0.5
 
-
 import rhinoscriptsyntax as rs  # type: ignore
 
-from compas_rv.datastructures import ThrustDiagram
-from compas_rv.scene import RhinoThrustObject
 from compas_rv.session import RVSession
 
 
 def RunCommand():
     session = RVSession()
 
-    thrust: RhinoThrustObject = session.scene.find_by_itemtype(ThrustDiagram)
+    form = session.find_formdiagram()
+    if not form:
+        return
+
+    force = session.find_forcediagram()
+    if not force:
+        return
+
+    thrust = session.find_thrustdiagram()
     if not thrust:
         return
 
@@ -23,25 +28,48 @@ def RunCommand():
 
     rs.UnselectAllObjects()
 
-    options = ["VertexAttributes", "EdgeAttributes"]
+    options = ["VertexAttributes", "EdgeAttributes", "FaceAttributes", "MoveSupports"]
     option = rs.GetString("Modify the Thrust Diagram", strings=options)
     if not option:
         return
 
     if option == "VertexAttributes":
-        selectable = list(thrust.mesh.vertices())
-        selected = thrust.select_vertices(selectable)
+        thrust.show_vertices = list(thrust.diagram.vertices())
+        thrust.redraw_vertices()
+
+        selected = thrust.select_vertices()
+
         if selected:
             thrust.update_vertex_attributes(selected)
 
     elif option == "EdgeAttributes":
-        selectable = list(thrust.mesh.edges_where(_is_edge=True))
-        selected = thrust.select_edges(selectable)
+        thrust.show_edges = list(thrust.diagram.edges_where(_is_edge=True))
+        thrust.redraw_edges()
+
+        selected = thrust.select_edges()
+
         if selected:
             thrust.update_edge_attributes(selected)
 
+    elif option == "MoveSupports":
+        form.show_vertices = False
+        form.redraw_vertices()
+
+        thrust.show_vertices = list(thrust.diagram.vertices_where(is_support=True))
+        thrust.redraw_vertices()
+
+        selected = thrust.select_vertices()
+
+        if not selected:
+            return
+
+        thrust.move_vertices_direction(selected, direction="Z")
+
     else:
         raise NotImplementedError
+
+    if session.settings.autoupdate:
+        rs.MessageBox("Automatic equilibrium updates are not available yet.", title="Info")
 
     # =============================================================================
     # Update scene
@@ -49,19 +77,16 @@ def RunCommand():
 
     rs.UnselectAllObjects()
 
+    form.show_vertices = True
+    form.redraw_vertices()
+
     thrust.show_vertices = True
     thrust.show_free = False
     thrust.show_fixed = True
     thrust.show_supports = True
-    thrust.show_edges = True
-    thrust.clear()
-    thrust.draw()
+    thrust.show_edges = False
 
-    rs.Redraw()
-
-    # =============================================================================
-    # Save session
-    # =============================================================================
+    thrust.redraw()
 
     if session.settings.autosave:
         session.record(name="Modify Thrust Diagram")
