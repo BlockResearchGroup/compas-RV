7fc72a4086d31db6950e2d9f415950c888ac87db
tomvanmele <tom.v.mele@gmail.com>
2025-02-03 16:00:49 +0100

    Bump version to 0.2.0

--- src/compas_rv/__init__.py
+++ src/compas_rv/__init__.py
@@ -5,7 +5,7 @@ __author__ = ["tom van mele"]
 __copyright__ = "ETH Zurich - Block Research Group"
 __license__ = "MIT License"
 __email__ = "tom.v.mele@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 HERE = os.path.dirname(__file__)
