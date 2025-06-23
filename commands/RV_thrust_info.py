75e8a451f8c5598879efc909d1c7af948b9a7b8c
tomvanmele <tom.v.mele@gmail.com>
2025-06-23 21:34:04 +0200

    first attempt as mesh info

---
+++ commands/RV_thrust_info.py
@@ -0,0 +1,33 @@
+#! python3
+# venv: brg-csd
+# r: compas_rv>=0.9.1
+
+from compas_rui.forms.meshinfo import MeshInfoForm
+from compas_rv.session import RVSession
+
+
+def RunCommand():
+    session = RVSession()
+
+    thrust = session.find_thrustdiagram()
+    if not thrust:
+        print("There is no ThrustDiagram in the scene.")
+        return
+
+    form = MeshInfoForm(
+        thrust.diagram,
+        vertex_attr_names=["x", "y", "z", "px", "py", "pz", "is_support", "_rx", "_ry", "_rz"],
+        edge_attr_names=["q"],
+        face_attr_names=["_is_loaded"],
+        title="Thrust Diagram Info",
+    )
+
+    form.show()
+
+
+# =============================================================================
+# Run as main
+# =============================================================================
+
+if __name__ == "__main__":
+    RunCommand()
